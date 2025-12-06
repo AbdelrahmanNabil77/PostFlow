56d3b92a071d260dfd3b7c8af929df1e40b32071
LAPTOP-SOFGCEFF\Khater <abdelrahmann89@gmail.com>
2025-12-06 17:00:46 +0200

    added blog permissions and serializers

---
+++ blog/permissions.py
@@ -0,0 +1,27 @@
+from rest_framework import permissions
+
+class IsAuthorOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow authors of a blog post to edit or delete it.
+    """
+    
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        
+        # Write permissions are only allowed to the author of the blog post
+        return obj.author == request.user
+
+class IsAdminOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow admin users to edit or delete.
+    """
+    
+    def has_permission(self, request, view):
+        # Read permissions are allowed to any request
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        
+        # Write permissions are only allowed to admin users
+        return request.user and request.user.is_staff
---
+++ blog/serializers.py
@@ -0,0 +1,74 @@
+from rest_framework import serializers
+from .models import BlogPost, Category, Tag
+from django.contrib.auth.models import User
+
+class UserBriefSerializer(serializers.ModelSerializer):
+    full_name = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'full_name')
+    
+    def get_full_name(self, obj):
+        return f"{obj.first_name} {obj.last_name}"
+
+class CategorySerializer(serializers.ModelSerializer):
+    post_count = serializers.IntegerField(read_only=True)
+    
+    class Meta:
+        model = Category
+        fields = ('id', 'name', 'description', 'created_at', 'post_count')
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ('id', 'name', 'created_at')
+
+class BlogPostListSerializer(serializers.ModelSerializer):
+    author = UserBriefSerializer(read_only=True)
+    category = CategorySerializer(read_only=True)
+    tags = TagSerializer(many=True, read_only=True)
+    excerpt = serializers.SerializerMethodField()
+    read_time = serializers.SerializerMethodField()
+    
+    class Meta:
+        model = BlogPost
+        fields = (
+            'id', 'title', 'slug', 'excerpt', 'author', 'category',
+            'tags', 'published_date', 'status', 'view_count',
+            'is_featured', 'read_time', 'created_at'
+        )
+        read_only_fields = ('slug', 'published_date', 'view_count')
+    
+    def get_excerpt(self, obj):
+        return obj.content[:150] + '...' if len(obj.content) > 150 else obj.content
+    
+    def get_read_time(self, obj):
+        # Assuming average reading speed of 200 words per minute
+        word_count = len(obj.content.split())
+        return max(1, word_count // 200)
+
+class BlogPostDetailSerializer(serializers.ModelSerializer):
+    author = UserBriefSerializer(read_only=True)
+    category = serializers.PrimaryKeyRelatedField(
+        queryset=Category.objects.all()
+    )
+    tags = serializers.PrimaryKeyRelatedField(
+        queryset=Tag.objects.all(),
+        many=True,
+        required=False
+    )
+    
+    class Meta:
+        model = BlogPost
+        fields = '__all__'
+        read_only_fields = (
+            'slug', 'published_date', 'view_count',
+            'created_at', 'updated_at', 'author'
+        )
+    
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['category'] = CategorySerializer(instance.category).data
+        representation['tags'] = TagSerializer(instance.tags.all(), many=True).data
+        return representation
